86817885bf54062b3e30fa0c9231d295d598caaa
Elias Tsai <cdefcdef0102@gmail.com>
2021-09-28 16:40:16 +0800

    add help url

--- TodoListWebApi/Controllers/HelpController.cs
+++ TodoListWebApi/Controllers/HelpController.cs
@@ -13,21 +13,7 @@ namespace ToDoListApi.Controllers
         [Route("api/[Controller]")]
         public IActionResult Get()
         {
-            string helphelp =
-                "Help about Authenticate Pass\r\n" +
-                "Get Token => api/Authenticate?User=[]&Pwd=[] , then you Get Token & RefreshToken \r\n" +
-                "Refresh   => api/Authenticate/Refresh?User=[]&Refresh=[] \r\n" +
-                "Now must be add Token Headers ,any request\r\n\r\n" +
-                "Help about used Todolist \r\n" +
-                "url = ( api/Todo/[User] ) => Example ( api/Todo/Test1 )\r\n" +
-                "Get    ( )                              ,GetTodolist \r\n" +
-                "Post   (need Body=> Title & description), =>Create Success OR Create failed \r\n" +
-                "Put    (need Body=> Title & description), =>Update Success OR Update failed \r\n" +
-                "Delete (need Body=> Title)              , =>Delete Success OR Delete failed \r\n\r\n" +
-                "<Wearning>\r\n" +
-                "               Any Title cannot be repeated in ToDoList.\r\n" +
-                "               Must be add Token Headers ,any Request.\r\n" +
-                "<Wearning>";
+            string helphelp =" https://github.com/Eliasfunction/SideProjectSelf_TodoListWebApiAndJWT ";
             return Ok(helphelp);
         }
     }
